bd3b791ac5763d341e9e2ffb7175e0a1c84b1bdb
zakgilbert <gilbertz@hawaii.edu>
2020-01-25 01:18:03 -1000

    First commit

---
+++ SpaceShooter/Assets/Fighter12/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private string HORIZONTAL = "Horizontal";
+    private string VERTICAL = "Vertical";
+    public Vector3 transformPosition (Vector3 vector) {
+        transform.position = transform.position + vector * Time.deltaTime;
+        return vector;
+    }
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transformPosition (new Vector3 (Input.GetAxis (HORIZONTAL), 0.0f, 0.0f));
+        transformPosition (new Vector3 (0.0f, Input.GetAxis (VERTICAL), 0.0f));
+    }
+}
